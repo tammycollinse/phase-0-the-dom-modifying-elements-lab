52543e25c62853f6a73ccddc4ac05086ea25a835
Tammy <tammycollinse@gmail.com>
2022-02-03 11:39:39 -0500

    commit tree b53e3b171cae687343f88e3e5305edb876ca3db6

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const element = document.getElementById("main");
+element.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"; 
+
+newHeader.textContent = "Tammy is the champion";
+document.body.append(newHeader);l
